215c7bf5b56c6a8e183617be13c298a00f64ee9f
Jackson Manser <73801213+Jacksonmanser25@users.noreply.github.com>
2020-11-20 17:16:23 -0500

    Add files via upload
    Gah, it won't run!!! Someone screwed with my code!

---
+++ decoder.py
@@ -0,0 +1,37 @@
+#scrambledAlphaCode = "i p a c o b i h i e g  n  o  T  :  d  s  e  t  a  h  l  h  s"
+#                      0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23
+scrambledAlphaCode = "ipacobihiegnoT:dsetahlhs"
+print (scrambledAlphaCode[13])
+print (scrambledAlphaCode[7])f
+print (scrambledAlphaCode[8])
+print (scrambledAlphaCode[16])
+print (scrambledAlphaCode[8])
+print (scrambledAlphaCode[23])
+print (scrambledAlphaCode[18])
+print (scrambledAlphaCode[22])
+print (scrambledAlphaCode[17])
+print (scrambledAlphaCode[2])
+print (scrambledAlphaCode[21])
+print (scrambledAlphaCode[20])
+print (scrambledAlphaCode[2])
+print (scrambledAlphaCode[3])
+print (scrambledAlphaCode[4])
+print (scrambledAlphaCode[15])
+print (scrambledAlphaCode[17])
+print (scrambledAlphaCode[14])
+print (scrambledAlphaCode[5])
+print (scrambledAlphaCode[8])
+print (scrambledAlphaCode[11])
+print (scrambledAlphaCode[10])
+print (scrambledAlphaCode[12])
+
+
+
+
+
+
+
+
+
+
+
